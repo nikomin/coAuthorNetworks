cf48d158ae956ef9d28bd13d324de6975c67bc5f
Niko Komin <niko.komin@uni.lu>
2020-03-07 21:20:50 -0800

    Catch KeyError for entries with missing fields.

--- main.py
+++ main.py
@@ -7,25 +7,30 @@ with open('phd.bib') as bibtex_file:
 
 network=dict()
 for e in bib_database.entries:
-    list_authors=e["author"].split(" and ")
-    for a in  range(len(list_authors)):
-        for b in  range(a+1,len(list_authors)):
-            auth_a=splitname(latex_to_unicode(list_authors[a]))
-            auth_b=splitname(latex_to_unicode(list_authors[b]))
-            author_a_id=(auth_a["last"][0]+auth_a["first"][0])
-            author_b_id=(auth_b["last"][0]+auth_b["first"][0])
-            if(author_a_id != author_b_id):
-                if(not(author_a_id in network.keys())):
-                    network[author_a_id] = dict()
-                if(not(author_b_id in network[author_a_id])):
-                    network[author_a_id][author_b_id]= 0
-                if(not(author_b_id in network.keys())):
-                    network[author_b_id] = dict()
-                if(not(author_a_id in network[author_b_id])):
-                    network[author_b_id][author_a_id]= 0
-                network[author_a_id][author_b_id] = network[author_a_id][author_b_id] + 1
-                network[author_b_id][author_a_id] = network[author_b_id][author_a_id] + 1
-
+    try:
+         list_authors=e["author"].split(" and ")
+         for a in  range(len(list_authors)):
+             for b in  range(a+1,len(list_authors)):
+                 auth_a=splitname(latex_to_unicode(list_authors[a]))
+                 auth_b=splitname(latex_to_unicode(list_authors[b]))
+                 author_a_id=(auth_a["last"][0]+auth_a["first"][0])
+                 author_b_id=(auth_b["last"][0]+auth_b["first"][0])
+                 if(author_a_id != author_b_id):
+                     if(not(author_a_id in network.keys())):
+                         network[author_a_id] = dict()
+                     if(not(author_b_id in network[author_a_id])):
+                         network[author_a_id][author_b_id]= 0
+                     if(not(author_b_id in network.keys())):
+                         network[author_b_id] = dict()
+                     if(not(author_a_id in network[author_b_id])):
+                         network[author_b_id][author_a_id]= 0
+                     network[author_a_id][author_b_id] = network[author_a_id][author_b_id] + 1
+                     network[author_b_id][author_a_id] = network[author_b_id][author_a_id] + 1
+    except KeyError as e:
+            # entry without 'author' are ignored
+            print('KeyError %s, entry ignored.' %str(e))
+            pass
+    
 print("source,target,weight")
 for l in network.keys():
     for u in network[l].keys():
