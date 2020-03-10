678d23df1a960025efd4a6896c90da49147783ed
Niko Komin <niko.komin@uni.lu>
2020-03-09 22:01:53 -0700

    refactoring

--- main.py
+++ main.py
@@ -5,62 +5,78 @@ import bibtexparser
 from bibtexparser.customization import splitname
 from bibtexparser.latexenc import latex_to_unicode
 
-filename = 'phd.bib'
 
+def genAuthorIDs(authorNames):
+    """ Return a unique ID for each name in given list. """
+    authorIDs = []
+    for authorName in authorNames:
+        tmp = splitname( latex_to_unicode(authorName) )
+        authorIDs.append( tmp["last"][0]+tmp["first"][0] )
+    return authorIDs
+
+def addCoauthorEdge( authorNetwork, author_a_id, author_b_id):
+    """ Adds an edge between author_a and author_b to the authorNetwork."""
+    if author_a_id != author_b_id:
+        if not( author_a_id in authorNetwork.keys() ):
+            authorNetwork[author_a_id] = dict()
+        if not( author_b_id in authorNetwork[author_a_id] ):
+            authorNetwork[author_a_id][author_b_id] = 0
+        if not( author_b_id in authorNetwork.keys() ):
+            authorNetwork[author_b_id] = dict()
+        if not( author_a_id in authorNetwork[author_b_id] ):
+            authorNetwork[author_b_id][author_a_id] = 0
+        authorNetwork[author_a_id][author_b_id] += 1
+        authorNetwork[author_b_id][author_a_id] += 1
+    return
+
+def printResults( authorNetwork, authorList ):
+    """ Print authorList and -network. """
+    print( "======================================================================")
+    print( "author, papercount" )
+    for authorID in authorList.keys():
+        print( "%s,%i" %(authorID, authorList[authorID]["papercount"]) )
+
+    print( "======================================================================")
+    print( "source,target,weight" )
+
+    for author in authorNetwork.keys():
+        for coAuthor in authorNetwork[author].keys():
+            print(author+","+coAuthor+","+str(authorNetwork[author][coAuthor]))
+    return
+
+
+
+# read file
+filename = 'phd.bib'
 with open(filename) as bibtex_file:
     bib_database = bibtexparser.load(bibtex_file,parser = bibtexparser.bparser.BibTexParser(common_strings=True))
 
 
 
-def genAuthorID(authorName):
-    """ Return a unique ID given an authorName. """
-    fullName = splitname(latex_to_unicode(authorName))
-    return fullName["last"][0]+fullName["first"][0]
-    
-authorList=dict()
-authorNetwork=dict()
-ignoredEntriesCount = 0
+authorList = dict() # for number of papers for each author
+authorNetwork = dict() # for number of links between pairs of authors
+ignoredEntriesCount = 0 # entries in bib_database that did not have "author"-field
+
+# parse entries
 for e in bib_database.entries:
     try:
         list_authors=e["author"].split(" and ")
-        authorIDs = []
-        for authorName in list_authors:
-            authorIDs.append( genAuthorID(authorName) )
+        authorIDs = genAuthorIDs( list_authors )
 
-        for i, author_a_id in enumerate(authorIDs):
+        for i, author_a_id in enumerate( authorIDs ):
             if not( author_a_id in authorList.keys() ):
                 authorList[ author_a_id ] = {'papercount': 0}
             authorList[ author_a_id ]['papercount'] += 1
-            for j, author_b_id in  enumerate(authorIDs[i+1:]):
-                if author_a_id != author_b_id:
-                    if not( author_a_id in authorNetwork.keys() ):
-                        authorNetwork[author_a_id] = dict()
-                    if not( author_b_id in authorNetwork[author_a_id] ):
-                        authorNetwork[author_a_id][author_b_id] = 0
-                    if not( author_b_id in authorNetwork.keys() ):
-                        authorNetwork[author_b_id] = dict()
-                    if not( author_a_id in authorNetwork[author_b_id] ):
-                        authorNetwork[author_b_id][author_a_id] = 0
-                    authorNetwork[author_a_id][author_b_id] += 1
-                    authorNetwork[author_b_id][author_a_id] += 1
+            for author_b_id in authorIDs[i+1:]:
+                addCoauthorEdge( authorNetwork, author_a_id, author_b_id)
+                
     except KeyError as e:
         # entries without 'author' are ignored
         ignoredEntriesCount += 1
         pass
 
-    
-
+# print results
 print( "# total entries: %i\n# ignored entries: %i"  %( len(bib_database.entries),ignoredEntriesCount ) )
 print( "# authors: %i" %len(authorNetwork.keys()) )
 
-print( "======================================================================")
-print( "author, papercount" )
-for authorID in authorList.keys():
-    print( "%s,%i" %(authorID, authorList[authorID]["papercount"]) )
-
-print( "======================================================================")
-print( "source,target,weight" )
-
-for author in authorNetwork.keys():
-    for coAuthor in authorNetwork[author].keys():
-        print(author+","+coAuthor+","+str(authorNetwork[author][coAuthor]))
+printResults( authorNetwork, authorList )
